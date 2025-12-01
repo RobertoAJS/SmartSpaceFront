8819176a5e99a306e2c883931415c1a668f9bbc9
rober <robertojs_03@hotmail.com>
2025-12-01 08:19:44 -0500

    html de ver ar

--- src/app/components/mueble/mueblelistar/mueblelistar.ts
+++ src/app/components/mueble/mueblelistar/mueblelistar.ts
@@ -41,12 +41,12 @@ export class Mueblelistar implements OnInit {
       this.displayedColumns = [
         'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 
         'c9',       // Subir 3D
-        'ver3d', // <--- Ver el modelo
+        'c12', // <--- Ver el modelo
         'c10', 'c11' //c10: Editar, c11: Eliminar
       ];
     } else {
       // Visitante (Solo lectura)
-      this.displayedColumns = ['c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'ver3d'];
+      this.displayedColumns = ['c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c12'];
     }
 
     this.cargarDatos();
