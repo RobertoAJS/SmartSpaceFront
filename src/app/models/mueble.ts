77d0ec2b30359f5a7795ce420dace5ebada0c937
rober <robertojs_03@hotmail.com>
2025-12-01 07:29:16 -0500

    implementando correcion por si resta algo

--- src/app/models/mueble.ts
+++ src/app/models/mueble.ts
@@ -1,8 +1,22 @@
+// Esta interfaz define qué datos trae la lista de diseños dentro del mueble
 export interface DisenoBasic {
   idDiseno: number;
   nombre: string;
   estado: string;
   fechaCreacion: string;
+}
+
+// --- Interfaces para la Búsqueda y Reportes ---
+export interface CriterioBusqueda {   //para enviar los filtros desde el formulario de busqueda
+  fechaInicio?: Date; // O string según tu manejo de fechas
+  fechaFin?: Date;
+  estado?: string;    // Ej: 'Activo', 'Borrador'
+}
+
+export interface ReporteDiseno {
+  totalDisenos: number;
+  cantidadPorEstado: { [key: string]: number }; // Ej: { "Activo": 10, "Archivado": 2 }
+  listaResultados: DisenoBasic[];
 }
 
 
