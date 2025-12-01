4ffd435d6bf5c50957d6cedb7c2e0ae663cd7583
rober <robertojs_03@hotmail.com>
2025-12-01 01:05:20 -0500

    ajuste en corse para permitir netlify

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -1,30 +1,28 @@
-import { inject } from "@angular/core";
-import { environment } from "../../environments/environments";
-import { Authservice } from "../services/authservice";
-import { HttpInterceptorFn } from "@angular/common/http";
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+
+import { Authservice } from '../services/authservice'; 
+import { environment } from '../../environments/environments';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
     const authService = inject(Authservice);
     const token = authService.getToken();
     const baseUrl = environment.base;
+
+    // Verificamos si la petición va hacia nuestro Backend.
+    // Esto es importante para NO enviar tu token a APIs externas (como Cloudinary o Google Maps)
     const isApiUrl = req.url.startsWith(baseUrl);
-
-    // --- DEBUG TEMPORAL ---
-    console.log('--- INTERCEPTOR ---');
-    console.log('1. URL Petición:', req.url);
-    console.log('2. Base URL esperada:', baseUrl);
-    console.log('3. ¿Coinciden?', isApiUrl);
-    console.log('4. ¿Hay token?', !!token);
-    // ----------------------
-
+    
+    // Si tenemos token y la petición es para nuestro backend, lo adjuntamos.
     if (token && isApiUrl) {
-        console.log('✅ Adjuntando Token...');
         const authReq = req.clone({
-            setHeaders: { Authorization: `Bearer ${token}` },
+            setHeaders: {
+                Authorization: `Bearer ${token}`,
+            },
         });
         return next(authReq);
     }
     
-    console.log('⚠️ Pasando SIN Token');
+    // Si no hay token o es una petición externa, pasa normal.
     return next(req);
 };
