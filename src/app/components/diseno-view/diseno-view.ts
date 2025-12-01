3295eca4bc667785dd2a2d47f7f2c55cf7fb2870
rober <robertojs_03@hotmail.com>
2025-12-01 08:29:03 -0500

    anulacion de modelo generico para mostrar modelo cargado

--- src/app/components/diseno-view/diseno-view.ts
+++ src/app/components/diseno-view/diseno-view.ts
@@ -1,24 +1,28 @@
 import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import '@google/model-viewer'; 
 import { Diseno } from '../../models/diseno';
 // 1. IMPORTAR EL SERVICIO (Asegúrate de que la ruta sea correcta)
 import { Disenoservice } from '../../services/disenoservice'; 
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-diseno-view',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule, 
+    MatProgressSpinnerModule, 
+    MatButtonModule, 
+    RouterLink
+  ],
   templateUrl: './diseno-view.html',
   styleUrls: ['./diseno-view.css'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA] 
 })
 export class DisenoViewComponent implements OnInit {
 
-
-  modeloUrl: string = 'https://modelviewer.dev/shared-assets/models/Astronaut.glb';
-  
   // Inicializamos vacío para evitar errores de "null" en el HTML
   diseno: Diseno = new Diseno(); 
   loading: boolean = true;
@@ -43,6 +47,10 @@ export class DisenoViewComponent implements OnInit {
       next: (data) => {
         this.diseno = data;
         this.loading = false;
+
+        console.log("Datos recibidos del Backend:", data);
+        console.log("URL del Modelo 3D:", data.urlModelo3d);
+
       },
       error: (err) => {
         console.error("Error al cargar el diseño:", err);
